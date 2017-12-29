9fa7f426eee0151d5f09b2f17ff98dfc50717b90
Egor Mischerin <e.mischerin@gmail.com>
2017-12-29 15:21:49 +0300

    Simple Password Generator

---
+++ PasswordGenerator/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace PasswordGenerator
+{
+        /// <summary>
+        /// Логика взаимодействия для MainWindow.xaml
+        /// </summary>
+        public partial class MainWindow:INotifyPropertyChanged
+        {
+                public event PropertyChangedEventHandler PropertyChanged;
+
+                
+
+                protected string password_length;
+
+                protected string password;
+
+                public string Password
+                {
+                        get { return password; }
+                        set
+                        {
+                                if (password != value)
+                                {
+                                        password = value;
+                                        OnPropertyChanged("Password");
+
+                                }
+                                        
+
+
+                        }
+                }
+                public string PasswordLength
+                {
+                        get { return password_length; }
+
+                        set
+                        {
+                                if (password_length != value)
+                                {
+                                        password_length = value;
+                                        OnPropertyChanged("PasswordLength");
+                                }
+                                
+                        }
+                }
+
+                public MainWindow()
+                {
+                        InitializeComponent();
+                        DataContext = this;
+                        
+                }
+
+                public void OnPropertyChanged(string property)
+                {
+                        if (PropertyChanged != null)
+                                PropertyChanged(this, new PropertyChangedEventArgs(property));
+                }
+
+                public void OnGenerateButtonClick(object sender, RoutedEventArgs e)
+                {
+                        NewPasswordGenerator passwordgenerator = new NewPasswordGenerator();
+                        Password = passwordgenerator.GeneratePassword(PasswordLength);
+
+                }
+
+        }
+}
---
+++ PasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace PasswordGenerator
+{
+        class NewPasswordGenerator:INotifyPropertyChanged
+        {
+                public event PropertyChangedEventHandler PropertyChanged;
+
+                protected const string symbols = "ABCDEFGHIGKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+                Random rand_number = new Random();
+
+                                      
+                
+                        
+
+                public string GeneratePassword(string password_length)
+                {
+                        bool _check_convert;
+
+                        int _length;
+
+                        _check_convert = Int32.TryParse(password_length,out _length);
+
+                        if (_check_convert == false)
+                                return "";
+
+                        if (_length < 0)
+                                _length = 1;
+
+                        string _new_password = "";
+
+                        char [] _symbols = symbols.ToCharArray();
+
+                        while (_new_password.Length != _length)
+                        {
+                                _new_password += _symbols[rand_number.Next(0, _symbols.Length - 1)]; 
+                        }
+
+                       return _new_password;
+
+                }
+
+                public void OnPropertyChanged(string property)
+                {
+                        if (PropertyChanged != null)
+                                PropertyChanged(this, new PropertyChangedEventArgs(property));
+                }
+
+                
+        }
+}
